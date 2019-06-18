9db78fe1caf8c4be65e4218eaf5d215d02bdfb69
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-18 16:39:51 +0300

    Initial commit

---
+++ CSharpWebSocketClientEgitim/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Quobject.SocketIoClientDotNet.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpWebSocketClientEgitim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var Socket = IO.Socket("http://localhost:3000");
+        }
+    }
+}
